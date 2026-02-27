f39ba0f4317bca590a1478b29154a5fa00418ddc
tinajoelly <tinaandrea.joelly2@edu.fh-kaernten.ac.at>
2026-02-27 23:54:59 +0100

    fix databases

--- core/settings.py
+++ core/settings.py
@@ -65,9 +65,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR}/db.sqlite3"  # fallback auf SQLite
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
